11ff80cb1aecb57c239f67e044381a1b5b5c147d
Saveliy <s.lekhtin@innopolis.university>
2023-11-09 11:25:12 +0300

    fix: change root router

--- src/api/users/routes.py
+++ src/api/users/routes.py
@@ -2,7 +2,7 @@ from src.api.dependencies import USER_SERVICE_DEPENDENCY
 from src.api.users import router
 
 
-@router.get("")
+@router.get("/")
 async def get_random_users(n: int, user_service: USER_SERVICE_DEPENDENCY):
     users = await user_service.get_random_users(n)
     return {"quantity": n, "users": users}
